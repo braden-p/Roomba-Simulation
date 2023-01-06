7e2b44db365717e2347ec0635b3dbffe0e1a5cf8
Braden Piper <bgpiper@gmail.com>
2023-01-05 22:28:10 -0600

    Write header
    Write a detailed explanation of what the program does

--- roomba_simulation.py
+++ roomba_simulation.py
@@ -1,15 +1,73 @@
-# 6.00.2x Problem Set 2: Simulating robots
+"""
+Roomba Simulation
+Created by Braden Piper, bradenpiper.com
+Created on Thu Jan 5, 2023
+Version = 1.1
+------------------------------------------
+DESCRIPTION:
+A program capable of simulating one or multiple roomba vacuum robots cleaning a
+rectangular room.
 
+This program uses Object Oriented Programming to simulate:
+    • A Rectangular Room
+        - A room consisting of clean or dirty tiles
+        - The room has a width and a height and contains (width * height) tiles.
+        At any particular time, each of these tiles is either clean or dirty.
+        - Initially, the entire floor is dirty. If a robot moves over a dirty
+        tile, the tile becomes clean.
+    • The Robots
+        - for the purposes of this program, the robots are points, and can pass
+        through each other or occupy the same point without interfering.
+        - At all times a robot has a particular position and direction in the room.
+        A robot also has a fixed speed.
+        - A robot may not move to a point outside of the room.
+        - A robot has a direction of motion represented by an integer d
+        satisfying 0 ≤ d < 360, which gives an angle in degrees.
+        - All robots move at the same speed s, a float, which is given and is
+        constant throughout the simulation. Every time-step, a robot moves in
+        its direction of motion by s units.
+        - If a robot detects that it will hit the wall within the time-step,
+        that time step is instead spent picking a new direction at random. The
+        robot will attempt to move in that direction on the next time step,
+        until it reaches another wall.
+    • Position
+        - A location within a two-dimensional room, used to keep track of the
+        location of the robots.
+        - The position is represented using coordinates (x, y), which are floats
+        satisfying 0 ≤ x < w and 0 ≤ y < h.
+
+The program demonstrates two different methods of movement by the robot(s):
+    (1) A Standard Movement Robot
+        - This robot attempts to move in its current direction; when it would
+        hit a wall, it instead chooses a new direction randomly.
+    (2) A Random Walk Robot
+        - This robot uses the "random walk" movement strategy: it
+        chooses a new direction at random at the end of each time-step.
+
+Termination
+The simulation ends when a specified fraction of the tiles in the room have been cleaned.
+------------------------------------------
+NOTE: This program was completed as part of the course MITx 6.00.2x - Introduction
+to Computational Thinking and Data Science. The general framework, and some
+of the functions were provided materials. The majority of the implementation is
+my own work.
+The provided materials include:
+    Position class
+    showPlot1 function
+    showPlot 2 function
+The other class names and function names were provided with docstrings, but the
+implementations are my own.
+"""
 import math
 import random
 
-import ps2_visualize
+import visualize
 import pylab
 
 # Edit the number in the line below based on which version of Python you have
-# eg. for Python 3.5, the number should be 35
+# for Python 3.5, the number should be 35
 # for Python 3.6, the number should be 36
-# for Python 3.9, the number should be 39 etc.
+# for Python 3.10, the number should be 310 etc.
 # This program will only work with Python versions 3.5 - 3.10
 
 from verify_movement39 import testRobotMovement
